5933fde4bc2bc1fbb321d6e71acbac160ba51617
Ryvan Firdy <ryvanfirdy@gmail.com>
2017-09-17 17:32:30 +0800

    add show new create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,22 @@ class BooksController < ApplicationController
    def index
 		@books = Book.all
    end
+
+   def show
+		@book = Book.find(params[:id])
+   end
+   def new
+		@book = Book.new
+   end
+   def create
+		   book = Book.new(book_params)
+		if book.save	
+			redirect_to books_path
+		else
+			render :new
+		end
+   end
+   private def book_params
+   params.require(:book).permit(:name, :author, :price, :quantity)
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/books', to: 'books#index'
+  get '/book/:id', to: 'books#show'
+  get '/books/new', to: 'books#new'
+  post '/books', to: 'books#create'
 end
